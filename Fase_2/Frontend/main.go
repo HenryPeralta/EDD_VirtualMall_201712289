6e7c87fe60bad30df76681e874177b4f66ae9fed
HenryPeralta <henrygabrielperalta@gmail.com>
2021-03-16 22:24:03 -0600

    Agregando Arbol AVL
    Agregando Arbol AVL

--- Fase_2/Frontend/main.go
+++ Fase_2/Frontend/main.go
@@ -8,12 +8,14 @@ import(
 	"encoding/json"
 	//"fmt"
 	"io/ioutil"
+	"./avl"
 	//"reflect"
 	//"os"
 	//"os/exec"
 	//"strconv"
 )
 
+// json de Tiendas
 type json_file struct {
 	Datos []struct {
 		Indice        string `json:"Indice"`
@@ -30,7 +32,46 @@ type json_file struct {
 	} `json:"Datos"`
 }
 
+//json de Inventarios
+type json_inventario struct {
+	Invetarios []struct {
+		Tienda       string `json:"Tienda"`
+		Departamento string `json:"Departamento"`
+		Calificacion int    `json:"Calificacion"`
+		Productos    []struct {
+			Nombre      string  `json:"Nombre"`
+			Codigo      int     `json:"Codigo"`
+			Descripcion string  `json:"Descripcion"`
+			Precio      float64 `json:"Precio"`
+			Cantidad    int     `json:"Cantidad"`
+			Imagen      string  `json:"Imagen"`
+		} `json:"Productos"`
+	} `json:"Invetarios"`
+}
+
 var dato_json json_file
+var dato_inventario json_inventario
+var Invent = avl.NewAVL()
+
+func avl_inventario(){
+	for i:=0; i < len(dato_inventario.Invetarios); i++ {
+		var productos = dato_inventario.Invetarios[i].Productos
+		//fmt.Println(tiendas)
+		for j:=0; j < len(productos); j++{
+			var product = productos[j]
+			//fmt.Println(product)
+			nuevoInventario := avl.Producto{
+				Nombre:      product.Nombre, 
+				Codigo:		 product.Codigo,      
+				Descripcion: product.Descripcion,
+				Precio:      product.Precio,      
+				Cantidad:    product.Cantidad,
+				Imagen:      product.Imagen,
+			}
+			Invent.Insertar(nuevoInventario)
+		}
+	}
+}
 
 func getTiendas(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -42,10 +83,24 @@ func createTiendas(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(body, &dato_json)
 }
 
+func getInventarios(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(dato_inventario)
+}
+
+func createInventarios(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &dato_inventario)
+	avl_inventario()
+	Invent.Print()
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/Tiendas", getTiendas).Methods("GET")
 	router.HandleFunc("/Tiendas", createTiendas).Methods("POST")
+	router.HandleFunc("/Inventarios", getInventarios).Methods("GET")
+	router.HandleFunc("/Inventarios", createInventarios).Methods("POST")
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
 	log.Println("Escuchando en http://localhost:3000")
 	http.ListenAndServe(":3000", router)
---
+++ Fase_2/Frontend/public/cargar_inventario.js
@@ -0,0 +1,14 @@
+//alert("hola");
+
+function obtener_json(){
+    var contenido = document.getElementById('consola_json').value;
+    console.log(contenido);
+
+    fetch('http://localhost:3000/Inventarios', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:contenido
+    })
+}
